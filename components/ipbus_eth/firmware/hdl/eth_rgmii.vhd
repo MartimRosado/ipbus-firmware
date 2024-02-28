ad770cca4492c4950c57df3da4afb3b985b582df
Martim Rosado <martim.rosado@outlook.com>
2024-02-28 17:30:41 +0100

    timing fixes to eth_rgmii and clock fixes for the HTG-940 board

--- components/ipbus_eth/firmware/hdl/eth_rgmii.vhd
+++ components/ipbus_eth/firmware/hdl/eth_rgmii.vhd
@@ -354,12 +354,21 @@ begin
 	rx_user_ef(0) <= rx_user_e;
 	rx_error <= rx_user_f(0);
 	
-	process(rx_clk)
-	begin
-		if rising_edge(rx_clk) then
-			rx_rst <= not rst;
-		end if;
-	end process;
+	 rst_cdc_inst : xpm_cdc_async_rst
+   generic map (
+      DEST_SYNC_FF => 2,    -- DECIMAL; range: 2-10
+      INIT_SYNC_FF => 0,    -- DECIMAL; 0=disable simulation init values, 1=enable simulation init values
+      RST_ACTIVE_HIGH => 0  -- DECIMAL; 0=active low reset, 1=active high reset
+   )
+   port map (
+      dest_arst => rx_rst, -- 1-bit output: src_arst asynchronous reset signal synchronized to destination
+                              -- clock domain. This output is registered. NOTE: Signal asserts asynchronously
+                              -- but deasserts synchronously to dest_clk. Width of the reset signal is at least
+                              -- (DEST_SYNC_FF*dest_clk) period.
+
+      dest_clk => rx_clk,     -- 1-bit input: Destination clock.
+      src_arst => not rst    -- 1-bit input: Source asynchronous reset signal.
+   );
 	
 	fifo: mac_fifo_axi4_2 port map(
 		m_axis_aclk => gtx_clk,
